85ce1dbc67183ae3e8e6ccb098c5c7399c3c1498
Tony Pujals <tony@subfuzion.com>
2014-10-12 18:27:31 -0700

    ran out of time, will fix delete later

--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 var assert = require('assert'),
 		db = require('../lib');
 
-
-
 describe('basic mongo tests', function() {
 
 	it('should connect to mongo localhost by default', function(done) {
@@ -20,5 +18,65 @@ describe('basic mongo tests', function() {
 		});
 
 	});
+
 });
 
+describe('basic crud tests', function() {
+	var _id;
+
+	after(function(done) {
+		db.deleteData({ _id: _id }, function(err, result) {
+			if (err) return done(err);
+			console.log(result);
+			done();
+		});
+	});
+
+	it ('should save a document', function(done) {
+
+		var data = {
+			paramX: 50,
+			paramY: 100
+		};
+
+		db.saveData(data, function(err, result) {
+			if (err) return done(err);
+
+			console.log(result);
+			_id = result[0]._id.toString();
+			console.log(_id);
+
+			done();
+
+		});
+
+	});
+
+	it ('should find a document', function(done) {
+
+		var query = {
+			paramX: 50
+		};
+
+		db.findOneData(query, function(err, result) {
+			if (err) return done(err);
+
+			console.log(result);
+			assert.equal(result._id.toString(), _id, "id didn't match");
+
+			done();
+		});
+	});
+
+	it.skip ('should update a document', function(done) {
+
+		done(assert(false));
+	});
+
+	it.skip ('should delete a document', function(done) {
+
+		done(assert(false));
+	});
+
+
+});
