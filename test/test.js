d2bab63604fcdde928bd8e6f55bae26420f87262
Tony Pujals <tony@subfuzion.com>
2014-10-12 21:10:43 -0700

    fixes delete test

--- test/test.js
+++ test/test.js
@@ -1,82 +1,78 @@
 var assert = require('assert'),
-		db = require('../lib');
+    db = require('../lib');
 
 describe('basic mongo tests', function() {
 
-	it('should connect to mongo localhost by default', function(done) {
+  it('should connect to mongo localhost by default', function(done) {
 
-		db.connect(function(err) {
-			done(err);
-		});
+    db.connect(function(err) {
+      done(err);
+    });
 
-	});
+  });
 
-	it('should connect to mongo localhost', function(done) {
+  it('should connect to mongo localhost', function(done) {
 
-		db.connect('localhost', function(err) {
-			done(err);
-		});
+    db.connect('localhost', function(err) {
+      done(err);
+    });
 
-	});
+  });
 
 });
 
 describe('basic crud tests', function() {
-	var _id;
+  var _id;
 
-	after(function(done) {
-		db.deleteData({ _id: _id }, function(err, result) {
-			if (err) return done(err);
-			console.log(result);
-			done();
-		});
-	});
+  it('should save a document', function(done) {
 
-	it ('should save a document', function(done) {
+    var data = {
+      paramX: 50,
+      paramY: 100
+    };
 
-		var data = {
-			paramX: 50,
-			paramY: 100
-		};
+    db.saveData(data, function(err, result) {
+      if (err) return done(err);
 
-		db.saveData(data, function(err, result) {
-			if (err) return done(err);
+      console.log(result);
+      _id = result[0]._id;
+      console.log(_id);
 
-			console.log(result);
-			_id = result[0]._id.toString();
-			console.log(_id);
+      done();
 
-			done();
+    });
 
-		});
+  });
 
-	});
+  it('should find a document', function(done) {
 
-	it ('should find a document', function(done) {
+    var query = {
+      paramX: 50
+    };
 
-		var query = {
-			paramX: 50
-		};
+    db.findOneData(query, function(err, result) {
+      if (err) return done(err);
 
-		db.findOneData(query, function(err, result) {
-			if (err) return done(err);
+      console.log(result);
+      assert(result._id.equals(_id), "id didn't match");
 
-			console.log(result);
-			assert.equal(result._id.toString(), _id, "id didn't match");
+      done();
+    });
+  });
 
-			done();
-		});
-	});
+  it.skip('should update a document', function(done) {
 
-	it.skip ('should update a document', function(done) {
 
-		done(assert(false));
-	});
+    done(assert(false));
+  });
 
-	it.skip ('should delete a document', function(done) {
-
-		done(assert(false));
-	});
+  it('should delete a document', function(done) {
+    db.deleteData({_id: _id}, function(err, result) {
+      if (err) return done(err);
+      console.log(result);
+      done();
+    });
+  });
 
 
 });
